e3e637e42de36dad179cb155d385c85197272f85
Markku Tavasti <tavasti@iki.fi>
2020-02-05 22:30:18 +0200

    Contra Bass

--- makefonts.sh
+++ makefonts.sh
@@ -292,6 +292,105 @@ sfzgen.py -n 11 Cello/clpz2* -l "volume=0 tune=0 lovel=69 hivel=98" >> $FILE
 sfzgen.py -n 11 Cello/clpz3* -l "volume=0 tune=0 lovel=99 hivel=122" >> $FILE
 sfzgen.py -n 11 Cello/clpz4* -l "volume=0 tune=0 lovel=123 hivel=127" >> $FILE
 
+NAME='Contra Bass Expressivo'
+FILE=ContraBass_Expressivo.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 20 ContraBass/bslte1* -l "volume=0 tune=0 lovel=0 hivel=70" >> $FILE
+sfzgen.py -n 20 ContraBass/bslte2* -l "volume=0 tune=0 lovel=71 hivel=101" >> $FILE
+sfzgen.py -n 17 ContraBass/bsltf3* -l "volume=0 tune=0 lovel=102 hivel=120" >> $FILE
+sfzgen.py -n 17 ContraBass/bsltf4* -l "volume=0 tune=0 lovel=121 hivel=127" >> $FILE
+
+
+NAME='Contra Bass Expressivo Mod'
+FILE=ContraBass_Expressivo_Mod.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+amp_veltrack=0
+volume_oncc1=-24
+volume_curvecc1=2
+xf_cccurve=power
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 20 ContraBass/bslte1* -l "volume=0 tune=0 xfout_locc1=50 xfout_hicc1=70" >> $FILE
+sfzgen.py -n 20 ContraBass/bslte2* -l "volume=0 tune=0 xfin_locc1=51 xfin_hicc1=71 xfout_locc1=88 xfout_hicc1=101" >> $FILE
+sfzgen.py -n 17 ContraBass/bsltf3* -l "volume=0 tune=0 xfin_locc1=89 xfin_hicc1=102 xfout_locc1=110 xfout_hicc1=120" >> $FILE
+sfzgen.py -n 17 ContraBass/bsltf4* -l "volume=0 tune=0 xfin_locc1=111 xfin_hicc1=121" >> $FILE
+
+
+NAME="Contra Bass Pizzicato"
+FILE=ContraBass_Pizzicato.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=0
+set_cc72=25
+<global>
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+
+EOF
+sfzgen.py -n 21 ContraBass/bspz1* -l "volume=0 tune=0 lovel=0 hivel=58" >> $FILE
+sfzgen.py -n 21 ContraBass/bspz2* -l "volume=0 tune=0 lovel=59 hivel=88" >> $FILE
+sfzgen.py -n 21 ContraBass/bspz3* -l "volume=0 tune=0 lovel=89 hivel=120" >> $FILE
+sfzgen.py -n 21 ContraBass/bspz4* -l "volume=0 tune=0 lovel=121 hivel=127" >> $FILE
+
+
+NAME='Contra Bass Spiccato'
+FILE=ContraBass_Spiccato.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+// Bow direction with cc22
+// cc22 below 42, both directions
+// 43-85 up bow
+// 86-127 down bow
+label_cc22=Bow Direction
+label_cc73=Attack
+label_cc72=Release
+set_cc22=0
+set_cc73=0
+set_cc72=25
+<global>
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+
+EOF
+sfzgen.py -n 16 ContraBass/bssp1* -l "volume=0 tune=0 lovel=0 hivel=95 hicc22=42" >> $FILE
+sfzgen.py -n 16 ContraBass/bssp2* -l "volume=0 tune=0 lovel=96 hivel=127 hicc22=42" >> $FILE
+
+sfzgen.py -n 16 ContraBass/bssp1* -l "volume=0 tune=0 lovel=0 hivel=95 locc22=43 hicc22=85" >> $FILE
+sfzgen.py -n 16 ContraBass/bssp2* -l "volume=0 tune=0 lovel=96 hivel=127 locc22=43 hicc22=85" >> $FILE
+
+sfzgen.py -n 16 ContraBass/bssp1* -l "volume=0 tune=0 lovel=0 hivel=95 locc22=86" >> $FILE
+sfzgen.py -n 16 ContraBass/bssp2* -l "volume=0 tune=0 lovel=96 hivel=127 locc22=86" >> $FILE
+
 NAME='Contra Bass Staccato'
 FILE=ContraBass_Staccato.sfz
 echo $NAME
@@ -327,4 +426,160 @@ sfzgen.py -n 16 ContraBass/bsst1* -l "volume=0 tune=0 lovel=0 hivel=65 locc22=86
 sfzgen.py -n 16 ContraBass/bsst2* -l "volume=0 tune=0 lovel=66 hivel=98 locc22=86" >> $FILE
 sfzgen.py -n 16 ContraBass/bsst3* -l "volume=0 tune=0 lovel=99 hivel=117 locc22=86" >> $FILE
 
+
+NAME='Contra Bass Sustained'
+FILE=ContraBass_Sustained.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 17 $(ls ContraBass/bsltf1* | grep -v Rel) -l "volume=0 tune=0 lovel=0 hivel=54" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf2* | grep -v Rel) -l "volume=0 tune=0 lovel=55 hivel=85" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf3* | grep -v Rel) -l "volume=0 tune=0 lovel=86 hivel=111" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf4* | grep -v Rel) -l "volume=0 tune=0 lovel=112 hivel=127" >> $FILE
+
+sfzgen.py -n 17 $(ls ContraBass/bsltf1* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=0 hivel=54" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf2* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=55 hivel=85" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf3* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=86 hivel=111" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf4* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=112 hivel=127" >> $FILE
+
+NAME='Contra Bass Sustained Mod'
+FILE=ContraBass_Sustained_Mod.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+amp_veltrack=0
+volume_oncc1=-24
+volume_curvecc1=2
+xf_cccurve=power
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 17 $(ls ContraBass/bsltf1* | grep -v Rel) -l "volume=0 tune=0 xfout_locc1=30 xfout_hicc1=54" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf2* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=31 xfin_hicc1=55 xfout_locc1=70 xfout_hicc1=85" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf3* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=71 xfin_hicc1=86 xfout_locc1=98 xfout_hicc1=111" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf4* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=99 xfin_hicc1=112" >> $FILE
+
+sfzgen.py -n 17 $(ls ContraBass/bsltf1* | grep Rel) -l "volume=0 tune=0 xfout_locc1=30 xfout_hicc1=54" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf2* | grep Rel) -l "volume=0 tune=0 xfin_locc1=31 xfin_hicc1=55 xfout_locc1=70 xfout_hicc1=85" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf3* | grep Rel) -l "volume=0 tune=0 xfin_locc1=71 xfin_hicc1=86 xfout_locc1=98 xfout_hicc1=111" >> $FILE
+sfzgen.py -n 17 $(ls ContraBass/bsltf4* | grep Rel) -l "volume=0 tune=0 xfin_locc1=99 xfin_hicc1=112" >> $FILE
+
+
+
+NAME='Contra Bass Tremolo'
+FILE=ContraBass_Tremolo.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 19 $(ls ContraBass/bsto1* | grep -v Rel) -l "volume=0 tune=0 lovel=0 hivel=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto2* | grep -v Rel) -l "volume=0 tune=0 lovel=61 hivel=87" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto3* | grep -v Rel) -l "volume=0 tune=0 lovel=88 hivel=127" >> $FILE
+
+sfzgen.py -n 19 $(ls ContraBass/bsto1* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=0 hivel=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto2* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=61 hivel=87" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto3* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=88 hivel=127" >> $FILE
+
+NAME='Contra Bass Tremolo Mod'
+FILE=ContraBass_Tremolo_Mod.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+amp_veltrack=0
+volume_oncc1=-24
+volume_curvecc1=2
+xf_cccurve=power
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 19 $(ls ContraBass/bsto1* | grep -v Rel) -l "volume=0 tune=0 xfout_locc1=45 xfout_hicc1=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto2* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=46 xfin_hicc1=61 xfout_locc1=72 xfout_hicc1=87" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto3* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=73 xfin_hicc1=88" >> $FILE
+
+sfzgen.py -n 19 $(ls ContraBass/bsto1* | grep Rel) -l "volume=0 tune=0 xfout_locc1=45 xfout_hicc1=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto2* | grep Rel) -l "volume=0 tune=0 xfin_locc1=46 xfin_hicc1=61 xfout_locc1=72 xfout_hicc1=87" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto3* | grep Rel) -l "volume=0 tune=0 xfin_locc1=73 xfin_hicc1=88" >> $FILE
+
+NAME='Contra Bass Trem Sul Ponticello'
+FILE=ContraBass_TremSulPonticello.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 19 $(ls ContraBass/bsto1* | grep -v Rel) -l "volume=0 tune=0 lovel=0 hivel=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto2* | grep -v Rel) -l "volume=0 tune=0 lovel=61 hivel=109" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bsto3* | grep -v Rel) -l "volume=0 tune=0 lovel=110 hivel=127" >> $FILE
+
+sfzgen.py -n 19 $(ls ContraBass/bstp1* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=0 hivel=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bstp2* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=61 hivel=109" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bstp3* | grep Rel) -l "trigger=release volume=0 tune=0 lovel=110 hivel=127" >> $FILE
+
+NAME='Contra Bass Trem Sul Ponticello Mod'
+FILE=ContraBass_TremSulPonticello_Mod.sfz
+echo $NAME
+cat << EOF > $FILE
+// $NAME
+<control>
+label_cc73=Attack
+label_cc72=Release
+set_cc73=2
+set_cc72=10
+<global>
+amp_veltrack=0
+volume_oncc1=-24
+volume_curvecc1=2
+xf_cccurve=power
+ampeg_attack_oncc73=10
+ampeg_release_oncc72=10
+ampeg_decay=0.005
+EOF
+sfzgen.py -n 19 $(ls ContraBass/bstp1* | grep -v Rel) -l "volume=0 tune=0 xfout_locc1=45 xfout_hicc1=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bstp2* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=46 xfin_hicc1=61 xfout_locc1=90 xfout_hicc1=109" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bstp3* | grep -v Rel) -l "volume=0 tune=0 xfin_locc1=91 xfin_hicc1=110" >> $FILE
+
+sfzgen.py -n 19 $(ls ContraBass/bstp1* | grep Rel) -l "volume=0 tune=0 xfout_locc1=45 xfout_hicc1=60" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bstp2* | grep Rel) -l "volume=0 tune=0 xfin_locc1=46 xfin_hicc1=61 xfout_locc1=90 xfout_hicc1=109" >> $FILE
+sfzgen.py -n 19 $(ls ContraBass/bstp3* | grep Rel) -l "volume=0 tune=0 xfin_locc1=91 xfin_hicc1=110" >> $FILE
 
